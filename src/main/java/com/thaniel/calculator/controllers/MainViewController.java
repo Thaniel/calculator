f7f2e8869fe782a4bcada156347f284b17468024
Daniel Herraez <danielhp0905@gmail.com>
2025-03-29 15:48:23 +0100

    Resize graph-pane

--- src/main/java/com/thaniel/calculator/controllers/MainViewController.java
+++ src/main/java/com/thaniel/calculator/controllers/MainViewController.java
@@ -5,6 +5,7 @@ import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.StackPane;
